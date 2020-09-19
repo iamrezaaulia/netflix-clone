8965781916f4a9421f40b0fde0210fcbd148040a
iamrezaaulia <iam.rezaaulia@gmail.com>
2020-09-19 23:26:34 +0700

    create accordion

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { JumbotronContainer, FooterContainer } from './containers';
+import {
+	JumbotronContainer,
+	FaqsContainer,
+	FooterContainer,
+} from './containers';
 
 export default function App() {
 	return (
 		<>
 			<JumbotronContainer />
+			<FaqsContainer />
 			<FooterContainer />
 		</>
 	);
