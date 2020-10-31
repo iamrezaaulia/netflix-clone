e70642fc5fdfc0170da18b075b278120738fad50
iamrezaaulia <iam.rezaaulia@gmail.com>
2020-10-31 22:53:46 +0700

    remove firebase config

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBaIBNZESLWv2HvwVnKF2yWRMbsnsz6fxw",
-  authDomain: "netflix-clone-d03fd.firebaseapp.com",
-  databaseURL: "https://netflix-clone-d03fd.firebaseio.com",
-  projectId: "netflix-clone-d03fd",
-  storageBucket: "netflix-clone-d03fd.appspot.com",
-  messagingSenderId: "69145501351",
-  appId: "1:69145501351:web:a4a4b38de32d571ac30582"
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: ''
 };
 
 const firebase = Firebase.initializeApp(firebaseConfig);
